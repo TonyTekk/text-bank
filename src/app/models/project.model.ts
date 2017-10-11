f401909f8c167e606d56acaae133ebf830656f26
Антон Скрыпий <a.skrypii@concert.ua>
2017-10-11 17:31:52 +0300

    Add color to project model

--- src/app/models/project.model.ts
+++ src/app/models/project.model.ts
@@ -1,9 +1,15 @@
 export class ProjectModel {
     public id: string;
     public title: string;
+    public color: string;
 
     public constructor(item: any) {
         item.id ? this.id = item.id : this.id = '';
         item.title ? this.title = item.title : this.title = 'default';
+        item.color ? this.color = item.color : this.color = ProjectModel.generateColor();
+    }
+
+    private static generateColor(): string {
+        return  `rgba(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)}, 0.12)`;
     }
 }
