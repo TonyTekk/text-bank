03242fc0024f08dbd397c5303d8d680a1ee3c9eb
Антон Скрыпий <a.skrypii@concert.ua>
2017-10-11 05:27:22 +0300

    Add init animation

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,9 +1,31 @@
 // Angular
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { OnDestroy} from '@angular/core';
+
+// Animation
+import { trigger } from '@angular/animations';
+import { style } from '@angular/animations';
+import { animate } from '@angular/animations';
+import { transition } from '@angular/animations';
 
 @Component({
     selector: 'app-projects',
     templateUrl: './projects.component.html',
-    styleUrls: ['./projects.component.css']
+    styleUrls: ['./projects.component.css'],
+    animations: [
+        trigger('init', [
+            transition('* => *', [
+                style({transform: 'translateX(-100%)'}),
+                animate(300)
+            ]),
+        ]),
+    ],
 })
-export class ProjectsComponent { }
+export class ProjectsComponent  implements OnInit, OnDestroy {
+    // Animation triggers
+    public init = false;
+
+    public ngOnInit(): void {}
+    public ngOnDestroy(): void { }
+}
