32129e5afe565e6e8ebad7e6bbe4b305b512a64e
Антон Скрыпий <a.skrypii@concert.ua>
2017-10-17 19:27:47 +0300

    Refactoring article module

--- src/app/modules/article/article-add/article-add.component.ts
+++ src/app/modules/article/article-add/article-add.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+// Angular
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+// App
+import { ArticleService } from '../../../services/article.service';
+import { ArticleModel } from '../../../models/article.model';
+import { FadeInAnimation } from '../../../animations/fade-in.animation';
 
 @Component({
-  selector: 'app-article-add',
-  templateUrl: './article-add.component.html',
-  styleUrls: ['./article-add.component.css']
+    selector: 'app-article-add',
+    templateUrl: './article-add.component.html',
+    styleUrls: ['./article-add.component.css'],
+    animations: [
+        FadeInAnimation,
+    ],
 })
-export class ArticleAddComponent implements OnInit {
+export class ArticleAddComponent {
+    public form = new FormGroup({
+        title      : new FormControl('', [Validators.required, Validators.minLength(3)]),
+        description: new FormControl(''),
+        text       : new FormControl(''),
+    });
 
-  constructor() { }
+    public constructor(
+        private router: Router,
+        public articleService: ArticleService,
+    ) { }
 
-  ngOnInit() {
-  }
+    public submit(): void {
+        const article = new ArticleModel({
+            title: this.form.controls.title.value,
+            description: this.form.controls.description.value,
+            text: this.form.controls.text.value,
+        });
 
+        this.articleService.push(article)
+            .then(() => {
+                this.router.navigate(['/articles']);
+            });
+    }
 }
--- src/app/modules/article/article-edit/article-edit.component.ts
+++ src/app/modules/article/article-edit/article-edit.component.ts
@@ -1,15 +1,108 @@
-import { Component, OnInit } from '@angular/core';
+// Angular
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { OnDestroy} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+// Material
+import { MatDialog } from '@angular/material';
+
+// RxJs
+import { Subscription } from 'rxjs/Subscription';
+
+// App
+import { ArticleService } from '../../../services/article.service';
+import { ArticleModel } from '../../../models/article.model';
+import { ArticleRemoveComponent } from '../article-remove/article-remove.component';
+import { ShowAnimation } from '../../../animations/show.animation';
 
 @Component({
-  selector: 'app-article-edit',
-  templateUrl: './article-edit.component.html',
-  styleUrls: ['./article-edit.component.css']
+    selector: 'app-article-edit',
+    templateUrl: './article-edit.component.html',
+    styleUrls: ['./article-edit.component.css'],
+    animations: [
+        ShowAnimation,
+    ],
 })
-export class ArticleEditComponent implements OnInit {
+export class ArticleEditComponent implements OnInit, OnDestroy {
+    // Subscription
+    private paramsSubscription: Subscription;
+    private articleSubscription: Subscription;
 
-  constructor() { }
+    public article: ArticleModel;
+    public show = false;
+    public form = new FormGroup({
+        title      : new FormControl('', [Validators.required, Validators.minLength(5)]),
+        description: new FormControl(''),
+        text       : new FormControl(''),
+    });
 
-  ngOnInit() {
-  }
+    public constructor(
+        private router: Router,
+        private route: ActivatedRoute,
+        public dialog: MatDialog,
+        public articleService: ArticleService,
+    ) { }
 
+    public ngOnInit(): void {
+        this.paramsSubscription = this.route.params
+            .subscribe(params => {
+                if (params['articleId']) {
+                    this.articleSubscription = this.articleService.get(params['articleId'])
+                        .subscribe((article) => {
+                            if (article) {
+                                this.article = new ArticleModel(article);
+                                this.show = true;
+
+                                this.form = new FormGroup({
+                                    title      : new FormControl(this.article.title, [Validators.required, Validators.minLength(5)]),
+                                    description: new FormControl(this.article.description),
+                                    text       : new FormControl(this.article.text),
+                                });
+                            }
+                        });
+                }
+            });
+    }
+
+    public ngOnDestroy(): void {
+        this.paramsSubscription.unsubscribe();
+        this.articleSubscription.unsubscribe();
+    }
+
+    public submit(): void {
+        const article = {
+            id: this.article.id,
+            apiId: this.article.apiId,
+            projectId: this.article.projectId,
+            title: this.form.controls.title.value,
+            description: this.form.controls.description.value,
+            text: this.form.controls.text.value,
+        };
+
+        this.articleService.update(article)
+            .then(() => {
+                this.router.navigate(['/articles']);
+            });
+    }
+
+    public remove(): void {
+        if (this.article) {
+            const dialogRef = this.dialog.open(ArticleRemoveComponent, {});
+
+            dialogRef.afterClosed().subscribe(
+                (result: boolean) => {
+                    if (result) {
+                        this.articleService.remove(this.article)
+                            .then(() => {
+                                this.router.navigate(['/articles']);
+                            });
+                    }
+                });
+        }
+    }
 }
